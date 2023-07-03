3a35f311160c2dc1982c232442c4bb457d1121b1
dryofficer21 <ilyassfakhtaoui5@gmail.com>
2023-07-03 15:43:25 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	for (s[i] >= '\0'; i++)
+	i = 0;
+	while (s[i])
 	{
 		if (s[i] == c)
-			return (&s[i]);
+		{
+			return (s + i);
+		}
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
