3f346102d9a69549e22ef37b04123a6e642269ed
dryofficer21 <ilyassfakhtaoui5@gmail.com>
2023-07-13 08:00:35 -0700

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: intiger
+ * malloc_checked - cause normal process termination with a status value of 98
  *
- * Return: null
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
